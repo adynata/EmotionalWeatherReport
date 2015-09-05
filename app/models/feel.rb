b8853f06030a5acbd61bc7ae9e7865205b7630d0
adynata <clairewoods@gmail.com>
2015-09-05 15:17:58 -0700

    feels restructuring

--- app/models/feel.rb
+++ app/models/feel.rb
@@ -10,6 +10,7 @@
 #
 
 class Feel < ActiveRecord::Base
+  attr_accessor :raw_feelers
   validates :user, :feel, presence: true
   belongs_to :user
   has_many :connections
@@ -19,25 +20,44 @@ class Feel < ActiveRecord::Base
     source: :users
     )
 
+  def self.raw_feelers
+    File.readlines("#{Rails.root.to_s}/lib/assets/inventory.txt").map(&:chomp)
+  end
 
   def self.feels_tree
-    feelers = File.readlines("#{Rails.root.to_s}/lib/assets/inventory.txt").map(&:chomp)
-    feels_tree = Hash.new { |h, k| h[k] = [] }
+    feels_tree = Hash.new { |h, k| h[k] = {} }
+    feel_class = nil
     current_feel_category = nil
 
-    feelers.each do |feel|
-      if !feel.match(/\p{Lower}/)
+    raw_feelers.each do |feel|
+      if feel[0] == "_"
+        feel_class = feel[1..-1]
+        feels_tree[feel_class] = {}
+      elsif !feel.match(/\p{Lower}/)
         current_feel_category = feel
-        feels_tree[current_feel_category] = []
+        feels_tree[feel_class][current_feel_category] = []
       else feel.match(/\p{Lower}/)
-        feels_tree[current_feel_category] << feel
+        feels_tree[feel_class][current_feel_category] << feel
       end
     end
     feels_tree
   end
 
+
+  def self.feels_tree_neg
+    self.feels_tree["NEGATIVE"]
+  end
+
+  def self.feels_tree_pos
+    self.feels_tree["POSITIVE"]
+  end
+
   def self.all_feels
-    feels_tree.flatten(2).map(&:downcase)
+    feels_only = []
+    raw_feelers.each do |feel|
+      feels_only << feel if feel.match(/\p{Lower}/)
+    end
+    feels_only
   end
 
 end
