f335b45929de49b7f0dd07f91f3e3da9fe35cd69
Robin Yang <robinyone@gmail.com>
2015-09-27 16:26:00 -0700

    dark overlay background WORKS

--- app/frontend/javascripts/register.jsx
+++ app/frontend/javascripts/register.jsx
@@ -91,17 +91,19 @@ let register = React.createClass({
       <Modal.Header closeButton>
         <Modal.Title>Sign Up</Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        <div className="col-sm-6 col-sm-offset-3">
-          <div className="results"></div>
-          <form id="registrationForm" onSubmit={this.submit} className="form">
-            <Input type="email" id="email" groupClassName="group-class" labelClassName="label-class" label="Email*" hasFeedback required />
-            <Input type="text" id="location" groupClassName="group-class" labelClassName="label-class" label="Default Location" />
-            <PasswordInput />
-            <ButtonInput type="submit" value="Register" />
-          </form>
-         </div>
-        </Modal.Body>
+      <div className="modal-body overlay-bg owl-bg sm">
+        <div className="row">
+          <div className="col-sm-8 col-sm-offset-2">
+            <div className="results"></div>
+            <form id="registrationForm" onSubmit={this.submit} className="form">
+              <Input type="email" id="email" groupClassName="group-class" labelClassName="label-class" label="Email*" hasFeedback required />
+              <Input type="text" id="location" groupClassName="group-class" labelClassName="label-class" label="Default Location" />
+              <PasswordInput />
+              <ButtonInput type="submit" value="Register" />
+            </form>
+           </div>
+          </div>
+        </div>
         <Modal.Footer>
           <Button onClick={this.props.close}>Cancel</Button> <Button onClick={this.props.switchModal}>Sign In</Button>
         </Modal.Footer>
